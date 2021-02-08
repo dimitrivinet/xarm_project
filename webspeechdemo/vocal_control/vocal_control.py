5d34d2c2596c0733a281cbcd747f201a5a539782
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2021-02-08 13:35:29 +0100

    advancement

--- webspeechdemo/vocal_control/vocal_control.py
+++ webspeechdemo/vocal_control/vocal_control.py
@@ -3,7 +3,6 @@
 from alphabet import letters_v3
 from copy import deepcopy
 from xarm.wrapper import XArmAPI
-from speech_control import speech_recog_v1
 
 import signal
 import sys
@@ -17,6 +16,16 @@ default_pos = [151.378, -644.823]
 current_pos = [0, 0]
 R_THRESH = -439.877
 L_THRESH = -611.823
+arm = "dummy"
+
+letter_functions = {"a": letters_v3.A, "b": letters_v3.B, "c": letters_v3.C, "d": letters_v3.D, "e": letters_v3.E,
+                    "f": letters_v3.F, "g": letters_v3.G, "h": letters_v3.H, "i": letters_v3.I, "j": letters_v3.J,
+                    "k": letters_v3.K, "l": letters_v3.L, "m": letters_v3.M, "n": letters_v3.N, "o": letters_v3.O,
+                    "p": letters_v3.P, "q": letters_v3.Q, "r": letters_v3.R, "s": letters_v3.S, "t": letters_v3.T,
+                    "u": letters_v3.U, "v": letters_v3.V, "w": letters_v3.W, "x": letters_v3.X, "y": letters_v3.Y,
+                    "z": letters_v3.Z, " ": letters_v3.space, "Invalid": letters_v3.Invalid}
+
+
 
 def get_current_pos(default_pos: list()) -> list(): 
     current_pos = [0, 0]
@@ -126,96 +135,72 @@ def write(arm, to_write):
     # print(paths)
 
 
-current_pos = get_current_pos(default_pos)
-
-signal.signal(signal.SIGINT, sigint_handler)
 
 
-letter_functions = {"a": letters_v3.A, "b": letters_v3.B, "c": letters_v3.C, "d": letters_v3.D, "e": letters_v3.E,
-                    "f": letters_v3.F, "g": letters_v3.G, "h": letters_v3.H, "i": letters_v3.I, "j": letters_v3.J,
-                    "k": letters_v3.K, "l": letters_v3.L, "m": letters_v3.M, "n": letters_v3.N, "o": letters_v3.O,
-                    "p": letters_v3.P, "q": letters_v3.Q, "r": letters_v3.R, "s": letters_v3.S, "t": letters_v3.T,
-                    "u": letters_v3.U, "v": letters_v3.V, "w": letters_v3.W, "x": letters_v3.X, "y": letters_v3.Y,
-                    "z": letters_v3.Z, " ": letters_v3.space, "Invalid": letters_v3.Invalid}
+def start():
+    arm = "dummy"
+    current_pos = get_current_pos(default_pos)
 
-arm = "dummy"
-connected = False
-while not connected:
-    try:
-        arm = XArmAPI('172.21.72.250', do_not_open=True)
-        arm.connect()
-        connected = True
-    except:
-        print("arm is not online. trying again in 3 seconds...")
-        time.sleep(3)
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    connected = False
+    while not connected:
+        try:
+            arm = XArmAPI('172.21.72.250', do_not_open=True)
+            arm.connect()
+            connected = True
+        except:
+            print("arm is not online. trying again in 3 seconds...")
+            time.sleep(3)
 
 
-# arm.set_world_offset([0, 0, 0, 0, 0, 0])
-arm.set_world_offset([0, 0, 0, 180, 0.6, -150.8])
-time.sleep(0.5)
+    # arm.set_world_offset([0, 0, 0, 0, 0, 0])
+    arm.set_world_offset([0, 0, 0, 180, 0.6, -150.8])
+    time.sleep(0.5)
 
-print(arm.position)
-time.sleep(1)
+    print(arm.position)
+    time.sleep(1)
 
-arm.motion_enable(enable=True)
-arm.set_mode(0)
-arm.set_state(state=0)
+    arm.motion_enable(enable=True)
+    arm.set_mode(0)
+    arm.set_state(state=0)
 
-# input()
+    # input()
 
-goto_current_pos(arm, current_pos)
-print("arm at starting pos")
-time.sleep(1)
-# input()
+    goto_current_pos(arm, current_pos)
+    print("arm at starting pos")
+    time.sleep(1)
+    # input()
 
-# base_pos = deepcopy(arm.position)
-# print("base_pos: {}".format(base_pos))
-# print("world offset: {}".format(arm.world_offset))
-# arm.set_world_offset([0, 0, 0, -179.7, base_pos[4], base_pos[5]])
-# time.sleep(0.5)
-# print("world offset: {}".format(arm.world_offset))
+    # base_pos = deepcopy(arm.position)
+    # print("base_pos: {}".format(base_pos))
+    # print("world offset: {}".format(arm.world_offset))
+    # arm.set_world_offset([0, 0, 0, -179.7, base_pos[4], base_pos[5]])
+    # time.sleep(0.5)
+    # print("world offset: {}".format(arm.world_offset))
 
-# arm.motion_enable(enable=True)
-# arm.set_mode(0)
-# arm.set_state(state=0)
+    # arm.motion_enable(enable=True)
+    # arm.set_mode(0)
+    # arm.set_state(state=0)
 
-# print(arm.position)
-# arm.set_tool_position(z=-5, wait=True, speed=10, mvacc=100)
+    # print(arm.position)
+    # arm.set_tool_position(z=-5, wait=True, speed=10, mvacc=100)
 
-threading.Thread(target=letters_v3.move, args=(arm,), daemon=True).start()
+    threading.Thread(target=letters_v3.move, args=(arm,), daemon=True).start()
 
-while True:
-    if finished is True:
 
-        print("\n=================================================\n")
+def send_sentence(to_write):
+    sentence = to_write
 
-        if input("\nLancer une reconnaissance vocale ? (y/n)") == "y":
-            sentence = ""
-            rep = ""
-            while rep != "ok":
-                sentence = speech_recog_v1.return_sentence()
-                sentence = sentence.split("écri")
-                try:
-                    print("to write: {}".format(sentence[1][2:]))
-                    rep = input("ok pour continuer, autre pour réessayer\n")
-                except:
-                    print("erreur: utiliser le mot clé 'écris' pour faire écrire le robot")
-                    try_again = input("recommencer? (y/n)")
+    sentence = ''.join((c for c in unicodedata.normalize(
+        'NFD', sentence[1][2:]) if unicodedata.category(c) != 'Mn'))
 
-                    if try_again != "y":
-                        exit(arm)
-                    else:
-                        time.sleep(0.5)
+    to_write = list(sentence)
+    print("writing: {}".format(sentence))
+    
+    finished = False
+    finished = write(arm, to_write)
 
-            sentence = ''.join((c for c in unicodedata.normalize(
-                'NFD', sentence[1][2:]) if unicodedata.category(c) != 'Mn'))
-
-            to_write = list(sentence)
-            print("writing: {}".format(sentence))
-            
-            finished = False
-            finished = write(arm, to_write)
-
-        else:
-            exit(arm)
-            
+def exit():
+    exit(arm)
+                
--- webspeechdemo/webspeechflask.py
+++ webspeechdemo/webspeechflask.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 
 import json
+import vocal_control
 
 
 app = Flask(__name__)
 @app.route('/', methods = ['POST', 'GET'])
 def student():
     if request.method == 'POST':
-      result = json.loads(request.data)["text"]
-      print(result)
+      result = json.loads(request.data)["text"] # get sentence to write
+      vocal_control.write(result)                             # send sentence to program
     return render_template('webspeechdemo/webspeechdemo.html')
 
 
