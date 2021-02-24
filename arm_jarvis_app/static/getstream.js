4649b23050ba0e23fb7a439ec7e60739fed91665
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2021-02-24 14:10:22 +0100

    added project web page

---
+++ arm_jarvis_app/static/getstream.js
@@ -0,0 +1,130 @@
+
+
+var recording = false;
+var recordButton = document.getElementById("record");
+var soundStream;
+var mediaRecorder;
+var timeslice = 500;
+
+var rec;
+var input;
+
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext;
+
+const socket = io('ws://localhost:8000');
+
+socket.on('connect', () => {
+  socket.send("connected to server")
+});
+
+
+const media_constraints = {
+  video: false,
+  audio: true,
+};
+
+const track_constraints = {
+  sampleRate: 44100,
+  channelCount: 1,
+  sampleSize: 16
+};
+
+const recorder_options = {
+  mimeType: "audio/wave;codecs=pcm"
+};
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+function startRecordFunc() {
+
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+
+  console.log("recording");
+  recording = true;
+
+  recordButton.innerHTML = "Recording...";
+
+  navigator.mediaDevices.getUserMedia(media_constraints)
+    .then(sendStream)
+    .catch(function (e) {
+      alert('getUserMedia() error: ' + e.name);
+      recording = false;
+    });
+}
+
+function sendStream(stream) {
+  console.log("sending stream to server");
+  soundStream = stream;
+
+  input = audioContext.createMediaStreamSource(stream);
+  // !console.log("debug1");
+
+  rec = new Recorder(input, {
+    numChannels: 1
+  });
+  //start the recording process 
+  // !console.log("debug2");
+  rec.record();
+
+  // soundStream.getTracks()[0].applyConstraints(track_constraints);
+  // console.log(soundStream.getTracks()[0].getConstraints());
+
+  // mediaRecorder = new MediaRecorder(stream, recorder_options);
+  // console.log(mediaRecorder.mimeType);
+  // mediaRecorder.start();
+
+  // console.log(mediaRecorder.state);
+  // console.log("recorder started");
+
+  // mediaRecorder.ondataavailable = function(e) {  
+  //   console.log(e.data);  
+  //   socket.emit('audio_stream', e.data);
+  // }
+
+}
+
+function stopRecordFunc() {
+  recordButton.innerHTML = "Record sound!";
+
+  rec.stop();
+  rec.exportWAV(function (blob) { 
+    console.log(blob.size);
+    socket.emit('audio_stream', blob) 
+  });
+
+  stopAudioStream(soundStream);
+  // mediaRecorder.stop();
+
+
+  console.log("stopping record");
+  socket.send("recording over.")
+  recording = false;
+}
+
+function stopAudioStream(stream) {
+  stream.getTracks().forEach(function (track) {
+    if (track.readyState == 'live' && track.kind === 'audio') {
+      track.stop();
+    }
+  });
+}
+
+function record() {
+  recording ? stopRecordFunc() : startRecordFunc();
+}
+
+recordButton.onclick = record;
+
+console.log("begin");
+
+if (hasGetUserMedia()) {
+  console.log("Good to go!");
+} else {
+  alert("getUserMedia() is not supported by your browser");
+  console.log("end");
+}
---
+++ arm_jarvis_app/xarm_jarvis.py
@@ -0,0 +1,99 @@
+
+import wave
+import os
+dirname = os.path.dirname(__file__)
+
+import sys
+sys.path.append(dirname)
+
+from voice_recog_vosk.voice_recog import recog 
+
+print(dirname)
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+#cors = CORS(app)
+
+socketio = SocketIO(app)
+
+HTTP_SERVER_PORT = 8000
+HTTP_SERVER_HOST = "localhost"
+
+sampleRate = 44100
+bitsPerSample = 16
+channels = 1
+
+numfile = 0
+
+def genHeader():
+    global sampleRate
+    global bitsPerSample
+    global channels
+
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
+
+@socketio.on('message')
+def handle_message(data):
+    if type(data) == str:
+        print('from client: ' + data)
+    else:
+        print(data)
+
+@socketio.on('connect')
+def connect():
+    print("client connected")
+
+@socketio.on('audio_stream')
+def handle_stream(data):
+    # print(data)
+    # print("end of byte stream")
+
+    # with open("test.wav", mode="wb") as f:
+    #     f.write(genHeader() + data)
+    global numfile
+    global dirname
+    # print(data)
+
+    with wave.open(f"{dirname}/wavs/file_{numfile}.wav", "wb") as f:
+        f.setnchannels(1)
+        f.setsampwidth(16 // 8)
+        f.setframerate(44100)
+        # f.setnframes(1)
+        f.setcomptype("NONE", "not compressed")
+        f.writeframes(genHeader() + data)
+    
+    print(f"file file_{numfile}.wav written")
+    recog(f"file_{numfile}.wav")
+
+    numfile += 1
+
+
+
+
+@app.route('/')
+def page():
+    return render_template('index.html', )
+
+
+if __name__ == '__main__':
+    print('server launched.')
+    print(dirname)
+    socketio.run(app, host=HTTP_SERVER_HOST, port = HTTP_SERVER_PORT)
