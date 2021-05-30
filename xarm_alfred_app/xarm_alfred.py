1f070fed0ae2cb94a9704beb277213c318d58111
dimitrivinet <dimitri.vinet@edu.devinci.fr>
2021-05-30 18:19:37 +0200

    beginning refactoring

--- xarm_alfred_app/xarm_alfred.py
+++ xarm_alfred_app/xarm_alfred.py
@@ -1,44 +1,39 @@
+
+
 import os
 import sys
-
-dirname = os.path.dirname(os.path.realpath(__file__))
-
-if dirname == "":
-    print("error: dirname empty")
-    sys.exit(0)
-
-print(dirname)
-sys.path.append(dirname)
-
-
-import wave
 import requests
-import argparse
-import threading
+import wave
+
+import azure.cognitiveservices.speech as speechsdk
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-# from voice_recog_vosk.voice_recog import recog 
-import azure.cognitiveservices.speech as speechsdk
-speech_config = speechsdk.SpeechConfig(subscription="b6aeeece6e0c404d8390528985e1a213", region="francecentral")
-speech_config.speech_recognition_language="fr-FR"
 
-parser = argparse.ArgumentParser(description='A.L.F.R.E.D., robotic assistant.')
-parser.add_argument('-n', '--no-robot', action='store_true', help="don't connect to the xArm")
-parser.add_argument('-r', '--reset-pos', action='store_true', help="reset the current position to default for writing mode")
-#TODO: this ^
-args = parser.parse_args()
-# print(args.no_robot)
+dirname = os.path.dirname(os.path.realpath(__file__))
+if dirname == "":
+    print("error: dirname empty")
+    sys.exit(0)
 
-# NO_ROBOT = args.no_robot
-RESET_POS = args.reset_pos
+WAV_SAVE_DIR = os.path.join(dirname, "wavs")
+if not os.path.isdir(WAV_SAVE_DIR):
+    os.makedirs(WAV_SAVE_DIR, exist_ok=True)
 
-NO_ROBOT = os.getenv("NO_ROBOT", default=0)
+
+speech_config = speechsdk.SpeechConfig(
+    subscription="b6aeeece6e0c404d8390528985e1a213", region="francecentral")
+speech_config.speech_recognition_language = "fr-FR"
+
+
+RESET_POS = int(os.getenv("RESET_POS", default=0))
+NO_ROBOT = int(os.getenv("NO_ROBOT", default=0))
 
 if not NO_ROBOT:
     import robot_control
+os.environ["RESET_POS"] = "1"
 
+RASA_URL = "http://0.0.0.0:5005/webhooks/rest/webhook"
 
 arm = "dummy"
 
@@ -47,7 +42,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 #cors = CORS(app)
 
-socketio = SocketIO(app, logger=False, engineio_logger=False, allow_upgrades=False)
+socketio = SocketIO(app, logger=False,
+                    engineio_logger=False, allow_upgrades=False)
 
 socketio.init_app(app, cors_allowed_origins="*")
 
@@ -57,32 +53,38 @@ HTTP_SERVER_HOST = "0.0.0.0"
 sampleRate = 44100
 bitsPerSample = 16
 channels = 1
-        
+
 
 def send_rasa(message: str) -> str:
-    r = requests.post("http://rasa:5005/webhooks/rest/webhook", json={"sender": "alfred_user", "message": message})
-    return r.json()[0]["text"]
+    r = requests.post(
+        RASA_URL, json={"sender": "alfred_user", "message": message})
+    try:
+        return r.json()[0]["text"]
+    except IndexError:
+        return f"#101 {r}"
+
 
 def genHeader():
     global sampleRate
     global bitsPerSample
     global channels
 
-    datasize = 2000*10**6
-    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
-    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
-    o += bytes("WAVE",'ascii')                                              # (4byte) File type
-    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
-    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
-    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
-    o += (channels).to_bytes(2,'little')                                    # (2byte)
-    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
-    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
-    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
-    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
-    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
-    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    datasize = 2000*10**6    
+    o = bytes("RIFF", 'ascii')                                              # (4byte) Marks file as RIFF    
+    o += (datasize + 36).to_bytes(4, 'little')                              # (4byte) File size in bytes excluding this and RIFF marker    
+    o += bytes("WAVE", 'ascii')                                             # (4byte) File type    
+    o += bytes("fmt ", 'ascii')                                             # (4byte) Format Chunk Marker    
+    o += (16).to_bytes(4, 'little')                                         # (4byte) Length of above format data    
+    o += (1).to_bytes(2, 'little')                                          # (2byte) Format type (1 - PCM)    
+    o += (channels).to_bytes(2, 'little')                                   # (2byte)    
+    o += (sampleRate).to_bytes(4, 'little')                                 # (4byte)    
+    o += (sampleRate * channels * bitsPerSample //8).to_bytes(4, 'little')  # (4byte)    
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)    
+    o += (bitsPerSample).to_bytes(2, 'little')                              # (2byte)    
+    o += bytes("data", 'ascii')                                             # (4byte) Data Chunk Marker    
+    o += (datasize).to_bytes(4, 'little')                                   # (4byte) Data size in bytes    
     return o
+
 
 @socketio.on('message')
 def handle_message(data):
@@ -91,74 +93,75 @@ def handle_message(data):
     else:
         print(data)
 
+
 @socketio.on('connect')
 def connect():
     print("client connected")
 
+
 @socketio.on('audio_stream')
 def handle_stream(data):
-    # print(data)
-    # print("end of byte stream")
 
-    global numfile
-    global dirname
-
-    with wave.open(f"{dirname}/wavs/file_0.wav", "wb") as f:
+    with wave.open(os.path.join(WAV_SAVE_DIR, "file_0.wav"), "wb") as f:
         f.setnchannels(1)
         f.setsampwidth(16 // 8)
         f.setframerate(44100)
         # f.setnframes(1)
         f.setcomptype("NONE", "not compressed")
         f.writeframes(genHeader() + data)
-    
+
     print(f"file written")
     print(f"recognizing...")
 
     audio_input = speechsdk.AudioConfig(filename=f"{dirname}/wavs/file_0.wav")
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
-    
+    speech_recognizer = speechsdk.SpeechRecognizer(
+        speech_config=speech_config, audio_config=audio_input)
+
     result = speech_recognizer.recognize_once_async().get()
 
     if result.reason == speechsdk.ResultReason.RecognizedSpeech:
         print("Recognized: {}".format(result.text))
-        emit('audio_stream_response', result.text)
+        stt = result.text
+        emit('audio_stream_response', stt)
     elif result.reason == speechsdk.ResultReason.NoMatch:
         print("No speech could be recognized: {}".format(result.no_match_details))
     elif result.reason == speechsdk.ResultReason.Canceled:
         cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        print("Speech Recognition canceled: {}".format(
+            cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-
-    print("_" * 16)
-    print("")
+            print("Error details: {}".format(
+                cancellation_details.error_details))
 
     rasa_response = send_rasa(stt)
-    # print(rasa_response)
-    emit('rasa_response', rasa_response[5:])
 
-    if not NO_ROBOT:
-        robot_control.rasa_command_queue.put(rasa_response)
-
-    # to_write = extract_to_write(rasa_response)
-    # if arm != "dummy" and to_write != -1:
-    #     arm.write(to_write)
-
-@socketio.on('manual_stream')
-def handle_manual_stream(data):
-    print(f"user: {data}")
-
-    rasa_response = send_rasa(data)
     print(f"rasa: {rasa_response}")
 
     emit('rasa_response', rasa_response[5:])
 
     if not NO_ROBOT:
         robot_control.rasa_command_queue.put(rasa_response)
+    
+    print("_" * 16)
+    print("")
 
-    # to_write = extract_to_write(rasa_response)
-    # if arm != "dummy" and to_write != -1:
-    #     arm.write(to_write)
+
+@socketio.on('manual_stream')
+def handle_manual_stream(data):
+    print(f"user: {data}")
+
+    rasa_response = send_rasa(data)
+
+    print(f"rasa: {rasa_response}")
+
+    emit('rasa_response', rasa_response[5:])
+
+    if not NO_ROBOT:
+        robot_control.rasa_command_queue.put(rasa_response)
+    
+    print("_" * 16)
+    print("")
+
 
 @app.route('/')
 def page():
@@ -168,9 +171,9 @@ def page():
 if __name__ == '__main__':
 
     if not NO_ROBOT:
-        arm = robot_control.Arm(reset_pos=RESET_POS, daemon=True, )
+        arm = robot_control.Arm(reset_pos=RESET_POS, daemon=True)
         arm.start()
 
-    print(f"\nstarting sever on {HTTP_SERVER_HOST}:{HTTP_SERVER_PORT}\n")
+    print(f"\nStarting sever on {HTTP_SERVER_HOST}:{HTTP_SERVER_PORT}\n")
 
-    socketio.run(app, host=HTTP_SERVER_HOST, port = HTTP_SERVER_PORT,)
+    socketio.run(app, host=HTTP_SERVER_HOST, port=HTTP_SERVER_PORT, debug=True)
